0fd87e3846dc20d53fce5fcd1f55dd720301419b
dermatologist <github@gulfdoctor.net>
2016-06-19 19:14:23 -0400

    before save change

--- DITagger/ImageWindow.py
+++ DITagger/ImageWindow.py
@@ -42,12 +42,12 @@ class ImageWindow(ImageModel.ImageModel, BaseWidget):
         self._buttonSave.value = self.__buttonSaveAction
 
     def __buttonSaveAction(self):
-        # self._usercomment['ditid'] = ControlText('Patient ID').value
-        # self._usercomment['lesion'] = ControlText('Lesion').value
-        # self._usercomment['diagnosis'] = ControlText('Diagnosis').value
-        # self._usercomment['location'] = ControlText('Location').value
-        # self._usercomment['ditcomment'] = ControlText('Comment').value
+        self._ditimage.value = self._ditpath + self._ditfile
         super(ImageModel.ImageModel, self).__setattr__('ditid', self._ditid.value)
+        super(ImageModel.ImageModel, self).__setattr__('lesion', self._lesion.value)
+        super(ImageModel.ImageModel, self).__setattr__('diagnosis', self._diagnosis.value)
+        super(ImageModel.ImageModel, self).__setattr__('location', self._location.value)
+        super(ImageModel.ImageModel, self).__setattr__('ditcomment', self._ditcomment.value)
         # In case the window has a parent
         # if self.parent!=None: self.parent.addPerson(self)
 
