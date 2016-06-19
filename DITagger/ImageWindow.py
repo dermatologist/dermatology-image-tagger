de3038adad314c4667b97c8701567b59422710d5
dermatologist <github@gulfdoctor.net>
2016-06-19 17:25:46 -0400

    Prior to refactor imageModel

--- DITagger/ImageWindow.py
+++ DITagger/ImageWindow.py
@@ -2,21 +2,37 @@ import pyforms
 from pyforms import BaseWidget
 from pyforms.Controls import ControlText
 from pyforms.Controls import ControlButton
-from   pyforms.Controls import ControlImage
+from pyforms.Controls import ControlImage
+
+from DITagger import ImageModel
+import os.path
 
 
-class ImageWindow(BaseWidget):
-    def __init__(self):
-        # Person.__init__(self, '', '', '')
+class ImageWindow(ImageModel.ImageModel, BaseWidget):
+    def __init__(self, path, file):
+        ImageModel.ImageModel.__init__(self, path, file)
         BaseWidget.__init__(self, 'Image window')
+        print(path + file)
 
         # Definition of the forms fields
         self._ditimage = ControlImage()
+        self._ditimage.value = path + file
+
         self._ditid = ControlText('Patient ID')
+        ControlText('Patient ID').value = self._usercomment['ditid']
+
         self._lesion = ControlText('Lesion')
+        ControlText('Lesion').value = self._usercomment['lesion']
+
         self._diagnosis = ControlText('Diagnosis')
+        ControlText('Diagnosis').value = self._usercomment['diagnosis']
+
         self._location = ControlText('Location')
-        self._ditcomment = ControlText('Comments')
+        ControlText('Location').value = self._usercomment['location']
+
+        self._ditcomment = ControlText('Comment')
+        ControlText('Comment').value = self._usercomment['ditcomment']
+
         self._buttonImage = ControlButton('Save Tags')
 
         self._formset = ['_ditimage', '_ditid', '_lesion', '_diagnosis', '_location', '_ditcomment', '_buttonImage',
