19dd2e22e2f2a27449432b494bc3e9a1d7abb7fd
dermatologist <github@gulfdoctor.net>
2016-06-21 22:09:37 -0400

    before glob2

--- DITagger/SearchWindow.py
+++ DITagger/SearchWindow.py
@@ -1,16 +1,16 @@
-import pyforms
 from pyforms import BaseWidget
-from pyforms.Controls import ControlText
 from pyforms.Controls import ControlButton
 from pyforms.Controls import ControlList
+from pyforms.Controls import ControlText
 
-from DITagger import ImageModel
-import os.path
+from DITagger import SettingsModel
 
 
-class SearchWindow(BaseWidget):
+class SearchWindow(SettingsModel.SettingsModel, BaseWidget):
     def __init__(self):
         BaseWidget.__init__(self, 'Search window')
+        SettingsModel.SettingsModel.__init__(self)
+        SettingsModel.SettingsModel.load()
 
         # Definition of the forms fields
         self._search = ControlText('Search')
@@ -22,9 +22,13 @@ class SearchWindow(BaseWidget):
         self._formset = ['_search', '_buttonSearch', '_results', 'by www.dermatologist.co.in']
 
         # Define the button action
-        # self._buttonSave.value = self.buttonSaveAction
+        self._buttonSearch.value = self.__buttonSearchAction
         # self._buttonLoad.value = self.buttonLoadAction
 
+    def __buttonSearchAction(self):
+        _folderList = self.get_setting('folders')
+        for _folder in _folderList:
+            return
         # In case the window has a parent
         # if self.parent!=None: self.parent.addPerson(self)
 
